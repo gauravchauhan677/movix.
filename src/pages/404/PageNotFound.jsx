428ffcd43118c34cea631902d11ebc974508b70b
Gaurav chauhan <gauravchauhan677@gmail.com>
2023-09-06 23:24:51 +0530

    movix app build.

---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.scss"
+
+const pageNotFound = () => {
+  return (
+    <div>
+      pagenotfound
+    </div>
+  )
+}
+
+export default pageNotFound
---
+++ src/store/homeSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+
+export const homeSlice = createSlice({
+  name: 'home',
+  initialState: {
+    url: {},
+    genres: {},
+  },
+  reducers: {
+  getApiCongifuration: (state, action) => {
+    state.url = action.payload
+  },
+
+  getGenres: (state, action) => {
+    state.genres = action.payload
+  }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { getApiCongifuration, getGenres } = homeSlice.actions
+
+export default homeSlice.reducer
